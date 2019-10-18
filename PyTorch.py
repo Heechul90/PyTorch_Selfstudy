5532bc069c044bada7bd8eda6381a9ac5ec52106
Heechul90 <heechul4296@gmail.com>
2019-10-18 15:58:25 +0900

    pytorch

---
+++ PyTorch.py
@@ -0,0 +1,120 @@
+
+# 1. CIFAR-10 로딩 및 정규화
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+)
+
+trainset = torchvision.datasets.CIFAR10(root = './data',
+                                        train = True,
+                                        download = True,
+                                        transform = transform)
+
+trainloader = torch.utils.data.DataLoader(trainset,
+                                          batch_size = 4,
+                                          shuffle = True,
+                                          num_workers = 2)
+
+testset = torchvision.datasets.CIFAR10(root = './data',
+                                       train = False,
+                                       download = True,
+                                       transform = transform)
+
+testloader = torch.utils.data.DataLoader(testset,
+                                         batch_size = 4,
+                                         shuffle = False,
+                                         num_workers = 2)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def imshow(img):
+    img = img /2 + 0.5
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+
+imshow(torchvision.utils.make_grid(images))
+
+print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+
+# 2. 컨볼루션 뉴럴 네트워크(합성곱 신경망) 정의
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc1 = nn.Linear(120, 84)
+        self.fc1 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+net = Net()
+
+
+# 3. 손실 함수 및 optimizer 정의
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(),
+                      lr = 0.001,
+                      momentum = 0.9)
+
+# 4. 네트워크 학습
+for epoch in range(2):
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        inputs, labels = data
+
+        optimizer.zero_grad()
+
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if i % 2000 == 1999:
+            print('[%d, %5d] loss: 5.3f' % epoch + 1, i + 1, running_loss / 2000)
+            running_loss = 0.0
+
+print('Finished Tranining')
+
+
+# 5. 평가 데이터를 이용한 네트워크 평가
+dataiter = iter(testloader)
+images, labels = dataiter.next()
+
+imshow(torchvision.utils.make_grid(images))
+print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+outpus = net(images)
+
+_, predicted = torch.max(outputs, 1)
+
+print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
+
